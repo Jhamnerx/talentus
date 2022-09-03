ab3342042f59e2754c549527929b0ae42ab28b10
Jhamner Sifuentes Vasquez <jhamnerx1x@gmail.com>
2022-09-03 15:34:42 -0500

    correciones menores

--- resources/views/livewire/admin/header/notificaciones.blade.php
+++ resources/views/livewire/admin/header/notificaciones.blade.php
@@ -12,17 +12,99 @@
         <div class="absolute top-0 right-0 w-2.5 h-2.5 bg-rose-500 border-2 border-white rounded-full">
         </div>
     </button>
-    <div class="origin-top-right z-10  absolute top-full right-0 -mr-48 sm:mr-0 min-w-80 bg-white border border-slate-200 py-1.5 rounded shadow-lg overflow-hidden mt-1"
+    <div class="origin-top-right z-10  sm:w-96 absolute top-full right-0 -mr-48 sm:mr-0 min-w-80 w-full bg-white border border-slate-200 py-1.5 rounded shadow-lg overflow-hidden mt-1"
         @click.outside="open = false" @keydown.escape.window="open = false" x-show="open"
         x-transition:enter="transition ease-out duration-200 transform"
         x-transition:enter-start="opacity-0 -translate-y-2" x-transition:enter-end="opacity-100 translate-y-0"
         x-transition:leave="transition ease-out duration-200" x-transition:leave-start="opacity-100"
         x-transition:leave-end="opacity-0" x-cloak>
-        <div class="text-xs font-semibold text-slate-400 uppercase pt-1.5 pb-2 px-4">Notifications {{ $count }}
+        <div class="text-xs font-semibold text-slate-400 uppercase pt-1.5 pb-2 px-4">Notificaciones {{ $count }}
         </div>
         <ul class="overflow-y-auto max-h-48 h-auto">
             @foreach ($notificaciones as $notificacion)
-                @if ($notificacion->type == 'App\Notifications\CrearRecordatorio')
+                {{-- <li class="border-b border-slate-200 last:border-0">
+
+                    <a class="block py-2 px-4 hover:bg-slate-50" href="#0" @click="open = false"
+                        @focus="open = true" @focusout="open = false">
+                        <span class="block text-sm mb-2">📣 <span class="font-medium text-slate-800">Edit
+                                your information in a swipe</span> Sint occaecat cupidatat non proident,
+                            sunt in culpa qui officia deserunt mollit anim.</span>
+
+                        <span class="block text-xs font-medium text-slate-400">Feb 9, 2021</span>
+                    </a>
+                    <div class="text-right mt-1">
+                        <a class="font-medium text-indigo-500 hover:text-indigo-600" href="#0">Action -&gt;</a>
+                    </div>
+                </li> --}}
+
+                <!-- Start -->
+                <div x-show="open" x-data="{ open: true }">
+                    <div
+                        class="border-b last:border-0 inline-flex flex-col w-full px-1 py-2 rounded-sm text-sm bg-white shadow-lg border border-slate-200 text-slate-600">
+                        <div
+                            class="flex w-full justify-between items-start hover:bg-slate-50 rounded-sm text-sm bg-white shadow-lg border border-slate-200">
+
+                            <a class="w-full py-2 " href="#0" @click="open = false" @focus="open = true"
+                                @focusout="open = false">
+                                <span class="block text-sm mb-2">📣
+                                    <span class="font-medium text-slate-800">
+                                        Edit
+                                        your information in a swipe
+                                    </span>
+                                    Sint occaecat cupidatat non proident,
+                                    sunt in culpa qui officia deserunt mollit anim.
+                                </span>
+
+                                <span class="block text-xs font-medium text-slate-400">Feb 9, 2021</span>
+
+                            </a>
+                            <button class="opacity-70 hover:opacity-80 ml-3 mt-[3px]" @click="open = false">
+                                <div class="sr-only">Close</div>
+                                <svg class="w-4 h-4 fill-current">
+                                    <path
+                                        d="M7.95 6.536l4.242-4.243a1 1 0 111.415 1.414L9.364 7.95l4.243 4.242a1 1 0 11-1.415 1.415L7.95 9.364l-4.243 4.243a1 1 0 01-1.414-1.415L6.536 7.95 2.293 3.707a1 1 0 011.414-1.414L7.95 6.536z" />
+                                </svg>
+                            </button>
+                        </div>
+
+                    </div>
+                    <!-- End -->
+                </div>
+                <!-- Start -->
+                <div x-show="open" x-data="{ open: true }">
+                    <div
+                        class="border-b last:border-0 inline-flex flex-col w-full px-1 py-2 rounded-sm text-sm bg-white shadow-lg border border-slate-200 text-slate-600">
+                        <div
+                            class="flex w-full justify-between items-start hover:bg-slate-50 rounded-sm text-sm bg-white shadow-lg border border-slate-200">
+
+                            <a class="w-full py-2 " href="#0" @click="open = false" @focus="open = true"
+                                @focusout="open = false">
+                                <span class="block text-sm mb-2">📣
+                                    <span class="font-medium text-slate-800">
+                                        Edit
+                                        your information in a swipe
+                                    </span>
+                                    Sint occaecat cupidatat non proident,
+                                    sunt in culpa qui officia deserunt mollit anim.
+                                </span>
+
+                                <span class="block text-xs font-medium text-slate-400">Feb 9, 2021</span>
+
+                            </a>
+                            <button class="opacity-70 hover:opacity-80 ml-3 mt-[3px]" @click="open = false">
+                                <div class="sr-only">Close</div>
+                                <svg class="w-4 h-4 fill-current">
+                                    <path
+                                        d="M7.95 6.536l4.242-4.243a1 1 0 111.415 1.414L9.364 7.95l4.243 4.242a1 1 0 11-1.415 1.415L7.95 9.364l-4.243 4.243a1 1 0 01-1.414-1.415L6.536 7.95 2.293 3.707a1 1 0 011.414-1.414L7.95 6.536z" />
+                                </svg>
+                            </button>
+                        </div>
+
+                    </div>
+                    <!-- End -->
+                </div>
+
+                {{-- @if ($notificacion->type == 'App\Notifications\CrearRecordatorio')
                     <li class="border-b border-slate-200 last:border-0" @click="open = false" @focus="open = true"
                         @focusout="open = false">
                         <div class="block py-2 px-4 hover:bg-slate-50">
@@ -51,7 +133,7 @@
                                 class="block text-xs font-medium text-slate-400">{{ $notificacion->created_at->diffForHumans() }}</span>
                         </a>
                     </li>
-                @endif
+                @endif --}}
             @endforeach
 
             {{-- <li class="border-b border-slate-200 last:border-0">
