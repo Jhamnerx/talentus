2beaa631b926e2482249d8c1fb9fd6f67bcc97d0
Jhamner Sifuentes Vasquez <jhamnerx1x@gmail.com>
2023-01-23 12:55:03 -0500

    validaciones de archivos pdf certificados

--- resources/views/pdf/acta.blade.php
+++ resources/views/pdf/acta.blade.php
@@ -3,11 +3,11 @@
 
 <head>
     @if ($acta->vehiculo && $acta->ciudades)
-    <title>ACTA {{ $acta->vehiculo->placa }}
-        {{ $acta->ciudades->prefijo . '-' . $acta->year . '-' . $acta->numero }}
-    </title>
+        <title>ACTA {{ $acta->vehiculo->placa }}
+            {{ $acta->ciudades->prefijo . '-' . $acta->year . '-' . $acta->numero }}
+        </title>
     @else
-    <title>Faltan datos en esta acta, por favor corrigelas</title>
+        <title>Faltan datos en esta acta, por favor corrigelas</title>
     @endif
 
 
@@ -167,172 +167,183 @@
 
 @if ($acta->fondo)
 
-<body background="data:image/jpeg;base64, {{ base64_encode(file_get_contents(asset('storage/' . $fondo))) }}">
+    <body background="data:image/jpeg;base64, {{ base64_encode(file_get_contents(asset('storage/' . $fondo))) }}">
     @else
 
-    <body>
-        @endif
+        <body>
+@endif
 
 
-        <div class="acta">
+<div class="acta">
 
-            <div class="header">
+    <div class="header">
 
 
 
-                <div class="numero">
-                    {{ $acta->codigo }}
-                </div>
+        <div class="numero">
+            {{ $acta->codigo }}
+        </div>
 
-            </div>
-            <div class="titulo">
-                <div class=" title">
-                    <span>ACTA DE INSTALACIÓN DE EQUIPO GPS</span>
-                </div>
+    </div>
+    <div class="titulo">
+        <div class=" title">
+            <span>ACTA DE INSTALACIÓN DE EQUIPO GPS</span>
+        </div>
 
-                @php
+        @php
 
-                $qr = base64_encode(
+            $qr = base64_encode(
                 QrCode::format('png')
-                ->size(120)
-                ->gradient(10, 88, 147, 5, 44, 82, 'vertical')
-                ->style('square')
-                ->eye('circle')
-                ->encoding('UTF-8')
-                ->generate(
-                ' VEHICULO: ' .
-                $acta->vehiculo->placa .
-                '|' .
-                " \n VALIDA: " .
-                $acta->inicio_cobertura->format('d-m-Y') .
-                '|' .
-                $acta->fin_cobertura->format('d-m-Y') .
-                "
-                \nCONSULTAR VALIDEZ EN: " .
-                route('consulta.actas', $acta->codigo),
-                ),
-                );
-                @endphp
-                <div class="qr">
-                    <img src="data:image/jpeg;base64, {{ $qr }}">
+                    ->size(120)
+                    ->gradient(10, 88, 147, 5, 44, 82, 'vertical')
+                    ->style('square')
+                    ->eye('circle')
+                    ->encoding('UTF-8')
+                    ->generate(
+                        ' VEHICULO: ' .
+                            $acta->vehiculo->placa .
+                            '|' .
+                            " \n VALIDA: " .
+                            $acta->inicio_cobertura->format('d-m-Y') .
+                            ' HASTA ' .
+                            $acta->fin_cobertura->format('d-m-Y') .
+                            "
+\nCONSULTAR VALIDEZ EN: " .
+                            route('consulta.actas', $acta->codigo),
+                    ),
+            );
 
-                </div>
-
-            </div>
-
-
-            <div class="certifica">
-                <div>
-                    <span>
-                        <b>{{ $plantilla->razon_social }}</b>, con RUC {{ $plantilla->ruc }}, Certifica que nuestro
-                        cliente:
-                        <b>{{ $acta->vehiculo->cliente ? strtoupper($acta->vehiculo->cliente->razon_social) : 'no
-                            existe' }}</b>
-                        con DNI/RUC: {{ $acta->vehiculo->cliente->numero_documento }}, ha adquirido un equipo
-                        GPS,
-                        para la unidad que se detalla a continuación:
-                        Así mismo a la fecha se encuentra transmitiendo a nuestra Plataforma de Monitoreo Satelital en
-                        tiempo real.
-                    </span>
-                </div>
-            </div>
-
-            <div class="descripcion">
-                <table class="tabla">
-                    <tr>
-                        <td height="20">Fecha de Instalación</td>
-                        <td height="20">: {{ $acta->inicio_cobertura->format('d-m-Y') }}</td>
-                    </tr>
-
-
-                    <tr>
-                        <td height="20">Modelo GPS</td>
-                        <td height="20">: {{ $acta->vehiculo->dispositivos->modelo->modelo }}</td>
-                    </tr>
-
-                    <tr>
-                        <td height="20">Imei</td>
-                        <td height="20">: {{ $acta->vehiculo->dispositivos->imei }}</td>
-                    </tr>
-
-                    <tr>
-                        <td height="20">Certificado de Homologación</td>
-                        <td height="20">: {{ $acta->vehiculo->dispositivos->modelo->certificado }} </td>
-                    </tr>
-
-                    <tr>
-                        <td height="20">Placa</td>
-                        <td height="20">: {{ $acta->vehiculo->placa }}</td>
-                    </tr>
-                    <tr>
-                        <td height="20">Marca</td>
-                        <td height="20">: {{ $acta->vehiculo->marca }}</td>
-                    </tr>
-                    <tr>
-                        <td height="20">Modelo</td>
-                        <td height="30">: {{ $acta->vehiculo->modelo }}</td>
-
-                    </tr>
-                    <tr>
-                        <td height="20">Tipo</td>
-                        <td height="20">: {{ $acta->vehiculo->tipo }}</td>
-
-                    </tr>
-                    <tr>
-                        <td height="20">A&ntilde;o</td>
-                        <td height="20">: {{ $acta->vehiculo->year }}</td>
-                    </tr>
-                    <tr>
-                        <td height="20">Color</td>
-                        <td height="20">: {{ $acta->vehiculo->color }}</td>
-                    </tr>
-                    <tr>
-                        <td height="20">Motor</td>
-                        <td height="20">: {{ $acta->vehiculo->motor }}</td>
-                    </tr>
-                    <tr>
-                        <td height="20">Serie</td>
-                        <td height="20">: {{ $acta->vehiculo->serie }}</td>
-                    </tr>
-                    <tr>
-                        <td height="20">Plataforma</td>
-                        <td height="20">: <b>{{ ucfirst($acta->plataforma) }}</b></td>
-
-                    </tr>
-                    <tr>
-                        <td height="20">Inicio Cobertura</td>
-                        <td height="20">: <b>{{ $acta->inicio_cobertura->format('d-m-Y') }}</b></td>
-
-                    </tr>
-                    <tr>
-                        <td height="20">Fin de cobertura</td>
-                        <td height="20">: <b>{{ $acta->fin_cobertura->format('d-m-Y') }}</b></td>
-                    </tr>
-                </table>
-
-            </div>
-
-            <div class="footer">
-                <div class="sello">
-                    @if ($acta->sello)
-                    <img src="data:image/jpeg;base64, {{ base64_encode(file_get_contents(asset('storage/' . $sello))) }}"
-                        alt="">
-                    @endif
-
-                </div>
-                <div class="fecha">
-                    <p>{{ $acta->fecha }}</p>
-                </div>
-            </div>
-
-            <div class="hash">
-                {{ $acta->unique_hash }}
-            </div>
+        @endphp
+        <div class="qr">
+            <img src="data:image/jpeg;base64, {{ $qr }}">
 
         </div>
 
+    </div>
 
 
-    </body>
+    <div class="certifica">
+        <div>
+            <span>
+                <b>{{ $plantilla->razon_social }}</b>, con RUC {{ $plantilla->ruc }}, Certifica que nuestro
+                cliente:
+                <b>{{ $acta->vehiculo->cliente
+                    ? strtoupper($acta->vehiculo->cliente->razon_social)
+                    : 'no
+                                                                                                                                                                            existe' }}</b>
+                con DNI/RUC:
+                {{ $acta->vehiculo->cliente ? $acta->vehiculo->cliente->numero_documento : 'REGISTRAR CLIENTE' }}, ha
+                adquirido un equipo
+                GPS,
+                para la unidad que se detalla a continuación:
+                Así mismo a la fecha se encuentra transmitiendo a nuestra Plataforma de Monitoreo Satelital en
+                tiempo real.
+            </span>
+        </div>
+    </div>
+
+    <div class="descripcion">
+        <table class="tabla">
+            <tr>
+                <td height="20">Fecha de Instalación</td>
+                <td height="20">: {{ $acta->inicio_cobertura->format('d-m-Y') }}</td>
+            </tr>
+
+
+            <tr>
+                <td height="20">Modelo GPS</td>
+                <td height="20">:
+                    {{ $acta->vehiculo->dispositivos ? $acta->vehiculo->dispositivos->modelo->modelo : '##COMPLETAR DATOS' }}
+                </td>
+            </tr>
+
+            <tr>
+                <td height="20">Imei</td>
+                <td height="20">:
+                    {{ $acta->vehiculo->dispositivos ? $acta->vehiculo->dispositivos->imei : '##COMPLETAR DATOS' }}
+                </td>
+            </tr>
+
+            <tr>
+                <td height="20">Certificado de Homologación</td>
+                <td height="20">:
+                    {{ $acta->vehiculo->dispositivos ? $acta->vehiculo->dispositivos->modelo->certificado : '##COMPLETAR DATOS' }}
+                </td>
+            </tr>
+
+            <tr>
+                <td height="20">Placa</td>
+                <td height="20">: {{ $acta->vehiculo->placa }}</td>
+            </tr>
+            <tr>
+                <td height="20">Marca</td>
+                <td height="20">: {{ $acta->vehiculo->marca }}</td>
+            </tr>
+            <tr>
+                <td height="20">Modelo</td>
+                <td height="30">: {{ $acta->vehiculo->modelo }}</td>
+
+            </tr>
+            <tr>
+                <td height="20">Tipo</td>
+                <td height="20">: {{ $acta->vehiculo->tipo }}</td>
+
+            </tr>
+            <tr>
+                <td height="20">A&ntilde;o</td>
+                <td height="20">: {{ $acta->vehiculo->year }}</td>
+            </tr>
+            <tr>
+                <td height="20">Color</td>
+                <td height="20">: {{ $acta->vehiculo->color }}</td>
+            </tr>
+            <tr>
+                <td height="20">Motor</td>
+                <td height="20">: {{ $acta->vehiculo->motor }}</td>
+            </tr>
+            <tr>
+                <td height="20">Serie</td>
+                <td height="20">: {{ $acta->vehiculo->serie }}</td>
+            </tr>
+            <tr>
+                <td height="20">Plataforma</td>
+                <td height="20">: <b>{{ ucfirst($acta->plataforma) }}</b></td>
+
+            </tr>
+            <tr>
+                <td height="20">Inicio Cobertura</td>
+                <td height="20">: <b>{{ $acta->inicio_cobertura->format('d-m-Y') }}</b></td>
+
+            </tr>
+            <tr>
+                <td height="20">Fin de cobertura</td>
+                <td height="20">: <b>{{ $acta->fin_cobertura->format('d-m-Y') }}</b></td>
+            </tr>
+        </table>
+
+    </div>
+
+    <div class="footer">
+        <div class="sello">
+            @if ($acta->sello)
+                <img src="data:image/jpeg;base64, {{ base64_encode(file_get_contents(asset('storage/' . $sello))) }}"
+                    alt="">
+            @endif
+
+        </div>
+        <div class="fecha">
+            <p>{{ $acta->fecha }}</p>
+        </div>
+    </div>
+
+    <div class="hash">
+        {{ $acta->unique_hash }}
+    </div>
+
+</div>
+
+
+
+</body>
 
 </html>
