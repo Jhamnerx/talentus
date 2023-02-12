720c53690d39722a7e52dbcc258a9f3bb1519794
Jhamner Sifuentes Vasquez <jhamnerx1x@gmail.com>
2023-02-12 10:42:35 -0500

    update fecha letras al descargar

--- app/Http/Controllers/Admin/PDF/ActaPdfController.php
+++ app/Http/Controllers/Admin/PDF/ActaPdfController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers\Admin\PDF;
 
-use App\Http\Controllers\Controller;
 use App\Models\Actas;
-use Barryvdh\DomPDF\Facade\Pdf;
+use App\Models\Ciudades;
 use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Support\Str;
+use App\Http\Controllers\Controller;
 
 class ActaPdfController extends Controller
 {
     public function __invoke(Actas $acta)
     {
+        $ciudad = Ciudades::find($acta->ciudades_id);
+        $acta->fecha =
+            $fecha = $ciudad->nombre . ", " . today()->day . " de " . Str::ucfirst(today()->monthName) . " del " . today()->year;
+        $acta->save();
         return $acta->getPDFData();
     }
     public function sendToMail(Actas $acta, $data)
