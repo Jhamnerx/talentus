49e35809129ba067729632b05294fb06426de618
Jhamner Sifuentes Vasquez <jhamnerx1x@gmail.com>
2023-02-12 10:43:32 -0500

    update fecha letras certs gps al descargar

--- app/Http/Controllers/Admin/PDF/CertificadoPdfController.php
+++ app/Http/Controllers/Admin/PDF/CertificadoPdfController.php
@@ -2,14 +2,20 @@
 
 namespace App\Http\Controllers\Admin\PDF;
 
-use App\Http\Controllers\Controller;
+use App\Models\Ciudades;
 use App\Models\Certificados;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Http\Controllers\Controller;
 
 class CertificadoPdfController extends Controller
 {
     public function __invoke(Certificados $certificado)
     {
+        $ciudad = Ciudades::find($certificado->ciudades_id);
+        $certificado->fecha =
+            $fecha = $ciudad->nombre . ", " . today()->day . " de " . Str::ucfirst(today()->monthName) . " del " . today()->year;
+        $certificado->save();
         return $certificado->getPDFData();
     }
 
