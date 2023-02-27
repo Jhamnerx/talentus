df342020c1ec21430ebdaec88bdf909c22c4735f
Jhamner Sifuentes Vasquez <jhamnerx1x@gmail.com>
2023-02-27 11:52:35 -0500

    fixed form edit input rol value of db error

--- resources/views/admin/usuarios/edit.blade.php
+++ resources/views/admin/usuarios/edit.blade.php
@@ -42,7 +42,7 @@
     <div class="md:grid md:grid-cols-3 md:gap-6">
         <div class="md:col-span-1 ml-4">
             <div class="px-6 sm:px-2">
-                <h3 class="text-lg font-medium leading-6 text-gray-900">Formulario para crear un usuario
+                <h3 class="text-lg font-medium leading-6 text-gray-900">Formulario para editar un usuario
                 </h3>
                 <p class="mt-1 text-sm text-gray-600">
                     Rellena los campos obligatorios
@@ -104,8 +104,10 @@
                             font-medium mb-1']),
                             ) !!}
 
+                            {!! Form::select('roles[]', $roles,
+                            $user->getRoleNames() ? $user->getRoleNames()[0] : NULL, ['class' => 'form-input
+                            w-2/4']) !!}
 
-                            {!! Form::select('roles[]', $roles, null, ['class' => 'form-input w-2/4']) !!}
                             @error('roles')
                             <p class="mt-2 peer-invalid:visible text-pink-600 text-sm">
                                 {{ $message }}
