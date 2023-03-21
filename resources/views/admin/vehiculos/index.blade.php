92e9e46239f924b8ef5ab944d1731a64d5893f92
Jhamner Sifuentes Vasquez <jhamnerx1x@gmail.com>
2023-03-21 18:44:44 -0500

    atajo savequick vehiculos  en vehiculos index

--- resources/views/admin/vehiculos/index.blade.php
+++ resources/views/admin/vehiculos/index.blade.php
@@ -9,6 +9,7 @@
     @livewire('admin.vehiculos.import')
     @livewire('admin.vehiculos.suspend')
     @livewire('admin.vehiculos.mantenimiento.save', ['update' => session('updated-numero')])
+    @livewire('admin.vehiculos.save-quick')
 
 
 @stop
