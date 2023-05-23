8aff12df8c8e1b7591831a022a41c1cff13ebab7
Jhamner Sifuentes Vasquez <jhamnerx1x@gmail.com>
2023-05-23 15:48:49 -0500

    add export function reviews

---
+++ app/Exports/ReviewsExport.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Cliente\Review;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use PhpOffice\PhpSpreadsheet\Cell\StringValueBinder;
+use Maatwebsite\Excel\Concerns\WithCustomValueBinder;
+
+class ReviewsExport extends StringValueBinder implements FromQuery, WithMapping, WithHeadings, WithCustomValueBinder, ShouldAutoSize
+{
+    use Exportable;
+
+    public function query()
+    {
+        return Review::query();
+    }
+    public function headings(): array
+    {
+        return [
+            '#',
+            'EMPRESA',
+            'NOMBRE',
+            'TELEFONO',
+            'FECHA DE NACIMIENTO',
+            '¿QUÉ TAN SATISFECHO SE SIENTE CON LA ATENCIÓN BRINDADA A SUS CONSULTAS Y REQUERIMIENTOS?',
+            '¿EN QUÉ ASPECTO CREE UD., QUE DEBERÍA MEJORAR NUESTRO SERVICIO?',
+            '¿TIENE ALGÚN INCONVENIENTE EN EL MANEJO DE NUESTRAS PLATAFORMAS DE MONITOREO?',
+            '¿TIENE ALGUNA SOLICITUD PENDIENTE QUE NO SE HAYA ATENDIDO?',
+            '¿RECOMENDARÍA NUESTRO SERVICIO A UN FAMILIAR O AMIGO?',
+        ];
+    }
+
+    public function map($review): array
+    {
+        return [
+            $review->id,
+            $review->empresa,
+            $review->name,
+            $review->telefono,
+            $review->birthday,
+            $review->question["q1"],
+            $review->question["q2"],
+            $review->question["q3"],
+            $review->question["q4"],
+            $review->question["q5"] . ", " . $review->question["q5_why"],
+        ];
+    }
+}
--- resources/views/livewire/admin/reviews/index.blade.php
+++ resources/views/livewire/admin/reviews/index.blade.php
@@ -40,7 +40,21 @@
                     </button>
                 </form>
 
+                <div class="relative inline-flex">
 
+                    <button wire:click="ExportReviews" wire:loading.class="bg-gray" wire:loading.attr="disabled"
+                        class="btn bg-emerald-600 hover:bg-emerald-700 text-white btn border-slate-200 hover:border-slate-300">
+                        <svg class="w-6 h-6 fill-current" viewBox="0 0 32 32">
+                            <path
+                                d="M16 20c.3 0 .5-.1.7-.3l5.7-5.7-1.4-1.4-4 4V8h-2v8.6l-4-4L9.6 14l5.7 5.7c.2.2.4.3.7.3zM9 22h14v2H9z" />
+                        </svg>
+                        <span class="hidden xs:block ml-2">Exportar</span>
+                    </button>
+
+                </div>
+                <div wire:loading wire:target="ExportReviews">
+                    Exportando...
+                </div>
 
                 <!-- Filter button -->
                 <div class="relative inline-flex">
