9a8dcbe59244c69ed000f84890285e9a3c9ac3f2
Shift <shift@laravelshift.com>
2023-11-14 21:21:39 +0000

    Use ES modules

--- public/js/cliente.js
+++ public/js/cliente.js
@@ -2102,7 +2102,7 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 // import Echo from 'laravel-echo';
 
-//window.Pusher = require("pusher-js");
+import Pusher from "pusher-js";
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,5 @@
-window._ = require("lodash");
+import _ from "lodash";
+window._ = _;
 // window.$ = require("jquery");
 
 /**
@@ -7,7 +8,8 @@ window._ = require("lodash");
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require("axios");
+import axios from "axios";
+window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
@@ -19,7 +21,7 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 // import Echo from 'laravel-echo';
 
-//window.Pusher = require("pusher-js");
+import Pusher from "pusher-js";
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
