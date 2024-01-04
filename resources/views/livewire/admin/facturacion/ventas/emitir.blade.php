03472d89b11f97deb51aae4ed17380204196f774
Jhamner Sifuentes Vasquez <jhamnerx1x@gmail.com>
2024-01-04 17:15:26 -0500

    cambiando boton con spinner - ventas emitir

--- resources/views/livewire/admin/facturacion/ventas/emitir.blade.php
+++ resources/views/livewire/admin/facturacion/ventas/emitir.blade.php
@@ -383,18 +383,9 @@
 
 
                     <div class="text-center md:text-right">
-                        <a wire:click.prevent="save"
-                            class="inline-flex overflow-hidden text-white bg-gray-900 rounded group cursor-pointer">
-                            <span
-                                class="px-3.5 py-2 text-white bg-purple-500 group-hover:bg-purple-600 flex items-center justify-center">
-                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path>
-                                </svg>
-                            </span>
-                            <span class="pl-4 pr-5 py-2.5 text-sm">EMITIR COMPROBANTE</span>
-                        </a>
+
+                        <x-form.button wire:click.prevent="save" spinner="save" label="EMITIR COMPROBANTE" black md
+                            icon="shopping-cart" />
                     </div>
                 </div>
             </div>
