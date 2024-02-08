37a6f20f199e582ba1ca8bcadc4bd584c499462a
Jhamner Sifuentes Vasquez <jhamnerx1x@gmail.com>
2024-02-08 12:50:40 -0500

    quitar debug de items

--- resources/views/components/admin/ventas/tabla-detalle-venta.blade.php
+++ resources/views/components/admin/ventas/tabla-detalle-venta.blade.php
@@ -77,7 +77,6 @@
                 </p>
             </tr>
             {{-- fila para añadir --}}
-            {{ json_encode($items) }}
             @if ($items->count() > 0)
                 @foreach ($items->all() as $clave => $item)
                     <tr wire:key="item-{{ $clave }}">
