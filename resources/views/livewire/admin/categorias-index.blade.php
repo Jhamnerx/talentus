4d2cb6aee2fdc0e6573bfe7abd8f96f194df400e
Jhamner Sifuentes Vasquez <jhamnerx1x@gmail.com>
2024-03-24 19:24:41 -0500

    delete laravel-blade-sortable de las vistas

--- resources/views/livewire/admin/categorias-index.blade.php
+++ resources/views/livewire/admin/categorias-index.blade.php
@@ -44,7 +44,7 @@
                 </form>
 
 
-
+                {{--
                 <!-- Filter button -->
                 <div class="relative inline-flex">
                     <button
@@ -55,7 +55,7 @@
                                 d="M9 15H7a1 1 0 010-2h2a1 1 0 010 2zM11 11H5a1 1 0 010-2h6a1 1 0 010 2zM13 7H3a1 1 0 010-2h10a1 1 0 010 2zM15 3H1a1 1 0 010-2h14a1 1 0 010 2z" />
                         </svg>
                     </button>
-                </div>
+                </div> --}}
 
                 <!-- Add customer button -->
                 @can('crear-categoria')
