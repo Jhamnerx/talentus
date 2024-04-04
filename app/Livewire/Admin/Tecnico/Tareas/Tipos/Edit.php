673619968ca3293ac1b490dbd8a2305e8ec62818
Jhamner Sifuentes Vasquez <jhamnerx1x@gmail.com>
2024-04-03 22:22:01 -0500

    edicion de tipo tarea actualizada

--- app/Livewire/Admin/Tecnico/Tareas/Tipos/Edit.php
+++ app/Livewire/Admin/Tecnico/Tareas/Tipos/Edit.php
@@ -2,14 +2,16 @@
 
 namespace App\Livewire\Admin\Tecnico\Tareas\Tipos;
 
-use App\Models\tipoTareas;
+use App\Models\User;
 use Livewire\Component;
+use App\Models\tipoTareas;
 
 class Edit extends Component
 {
 
     public $modalEdit = false;
     public $nombre, $costo = 0, $descripcion = "";
+    public $user_id = null;
     public $afecta_mantenimiento = false;
 
     public tipoTareas $tipo_tarea;
@@ -19,25 +21,34 @@ class Edit extends Component
 
     protected function rules()
     {
-        return [
+        $rules = [
             'nombre' => 'required',
             'costo' => 'required',
             'descripcion' => 'required',
             'afecta_mantenimiento' => 'boolean',
+            'user_id' => 'required',
         ];
+
+        if (auth()->user()->hasRole('tecnico')) {
+            $rules['user_id'] = 'nullable';
+        }
+
+        return $rules;
     }
     protected function messages()
     {
         return [
             'nombre.required' => 'Escribe una descripcion',
             'costo.required' => 'Ingresa un costo',
+            'user_id.required' => 'Selecciona un tecnico',
 
         ];
     }
 
     public function render()
     {
-        return view('livewire.admin.tecnico.tareas.tipos.edit');
+        $tecnicos = User::role('tecnico')->get();
+        return view('livewire.admin.tecnico.tareas.tipos.edit', compact('tecnicos'));
     }
 
 
@@ -49,6 +60,7 @@ class Edit extends Component
         $this->tipo_tarea = $tipo_tarea;
         $this->descripcion = $tipo_tarea->descripcion;
         $this->afecta_mantenimiento = $tipo_tarea->afecta_mantenimiento;
+        $this->user_id = $tipo_tarea->user_id;
         $this->modalEdit = true;
     }
     public function closeModal()
@@ -65,10 +77,14 @@ class Edit extends Component
     public function save()
     {
         $data = $this->validate();
+
+        if (auth()->user()->hasRole('tecnico')) {
+            $data['user_id'] = auth()->user()->id;
+        }
+
         try {
             $this->tipo_tarea->update($data);
-            $this->closeModal();
-            $this->dispatch('updateIndex');
+            $this->afterSave();
         } catch (\Throwable $th) {
             $this->dispatch(
                 'notify-toast',
@@ -77,5 +93,29 @@ class Edit extends Component
                 mensaje: 'Mensaje: ' . $th->getMessage() . "."
             );
         }
+    }
+    public function afterSave()
+    {
+        $this->dispatch(
+            'notify-toast',
+            icon: 'success',
+            title: 'TIPO TAREA ACTUALIZADA',
+            mensaje: 'Se actualizo correctamente el tipo de tarea ',
+        );
+
+        $this->closeModal();
+        $this->resetProp();
+        $this->dispatch('updateIndex');
+    }
+
+    public function resetProp()
+    {
+        $this->reset([
+            'nombre',
+            'costo',
+            'descripcion',
+            'user_id',
+            'afecta_mantenimiento',
+        ]);
     }
 }
