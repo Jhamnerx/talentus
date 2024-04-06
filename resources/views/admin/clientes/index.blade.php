a7d983970f21cb4e85083f059a260e3bcd7e249e
Jhamner Sifuentes Vasquez <jhamnerx1x@gmail.com>
2024-04-06 01:11:51 -0500

    eliminar scripts no usados en vista index

--- resources/views/admin/clientes/index.blade.php
+++ resources/views/admin/clientes/index.blade.php
@@ -11,63 +11,7 @@
 @stop
 
 @section('js')
-    @if (session('store'))
-        <script>
-            $(document).ready(function() {
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Guardado',
-                    text: '{{ session('store') }}',
-                    showConfirmButton: true,
-                    confirmButtonText: "Cerrar"
 
-                })
-            });
-        </script>
-    @endif
-    @if (session('export'))
-        <script>
-            $(document).ready(function() {
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Exportar',
-                    text: '{{ session('export') }}',
-                    showConfirmButton: true,
-                    confirmButtonText: "Cerrar"
-
-                })
-            });
-        </script>
-    @endif
-    @if (session('update'))
-        <script>
-            $(document).ready(function() {
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Actualizado',
-                    text: '{{ session('update') }}',
-                    showConfirmButton: true,
-                    confirmButtonText: "Cerrar"
-
-                })
-            });
-        </script>
-    @endif
-
-    @if (session('delete'))
-        <script>
-            $(document).ready(function() {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Eliminado',
-                    text: '{{ session('delete') }}',
-                    showConfirmButton: true,
-                    confirmButtonText: "Cerrar"
-
-                })
-            });
-        </script>
-    @endif
     <script>
         window.addEventListener('clientes-import', event => {
             iziToast.success({
