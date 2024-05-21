6b9c14a3ae0c2815633ec4dd835a6154709b18f6
Jhamner Sifuentes Vasquez <jhamnerx1x@gmail.com>
2024-05-21 09:25:30 -0500

    validacion de numero documento sin espacios

--- app/Http/Requests/ClientesRequest.php
+++ app/Http/Requests/ClientesRequest.php
@@ -36,6 +36,7 @@ class ClientesRequest extends FormRequest
                 'numero_documento' => [
                     'required',
                     'min:6',
+                    'alpha_num',
 
                     Rule::unique('clientes', 'numero_documento')->where(
                         fn ($query) =>
@@ -85,6 +86,7 @@ class ClientesRequest extends FormRequest
             'telefono.digits_between' => 'Ingresa como maximo 9 caracteres numericos',
             'telefono.numeric' => 'El numero de telefono debe ser un numero',
             'email.email' => 'Debe tener formato de correo electronico',
+            'numero_documento.alpha_num' => 'Solo se permiten caracteres alfanumericos',
         ];
     }
 }
