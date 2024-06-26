ba8d758aa37303d9f2403482f3e847371a1068de
Jhamner Sifuentes Vasquez <jhamnerx1x@gmail.com>
2024-06-26 16:19:24 -0500

    cambios

--- resources/views/components/admin/facturacion/tabla-detalle.blade.php
+++ resources/views/components/admin/facturacion/tabla-detalle.blade.php
@@ -97,7 +97,7 @@
                 </tr>
             @endforeach
             {{-- fila para añadir --}}
-            {{ json_encode($items) }}
+
             @foreach ($items->all() as $clave => $item)
                 <tr class="main bg-slate-100" wire:key="item-{{ $clave }}">
                     <td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
