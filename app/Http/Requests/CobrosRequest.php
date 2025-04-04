afa385e1b2aa239d0e8eb61d63b4cec394050e02
Jhamner Sifuentes Vasquez <jhamnerx1x@gmail.com>
2025-04-04 14:17:29 -0500

    peprmitir decimales en cobros detalle plan

--- app/Http/Requests/CobrosRequest.php
+++ app/Http/Requests/CobrosRequest.php
@@ -41,7 +41,7 @@ class CobrosRequest extends FormRequest
             "observacion" => 'nullable',
 
             'items' => 'array|between:1,100',
-            'items.*.plan' => 'required|integer',
+            'items.*.plan' => 'required|decimal:0,2',
             'items.*.placa' => 'required',
             'items.*.fecha' => 'required|date',
             'items.*.vehiculo_id' => 'required',
