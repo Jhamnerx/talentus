e7a26c9719bc23dda2743f77b9e9e7a95f9dfdca
Jhamner Sifuentes Vasquez <jhamnerx1x@gmail.com>
2025-05-07 11:34:47 -0500

    update

--- app/Notifications/EnviarMensajeCobro.php
+++ app/Notifications/EnviarMensajeCobro.php
@@ -17,7 +17,7 @@ class EnviarMensajeCobro extends Notification implements ShouldQueue
 
     public function __construct($mensaje)
     {
-        dd($mensaje);
+
         $this->mensaje = $mensaje;
     }
 
