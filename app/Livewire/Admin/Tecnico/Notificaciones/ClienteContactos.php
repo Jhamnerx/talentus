af1d5cadc3e5dd615fe0217a9bfd3ef045dd6745
Jhamner Sifuentes Vasquez <jhamnerx1x@gmail.com>
2025-05-13 11:22:33 -0500

    update whatsapp

--- app/Livewire/Admin/Tecnico/Notificaciones/ClienteContactos.php
+++ app/Livewire/Admin/Tecnico/Notificaciones/ClienteContactos.php
@@ -58,12 +58,22 @@ class ClienteContactos extends Component
 
                 if ($contacto->telefono) {
 
-                    $respuesta = $whatsApp->sendConfirmationClient($this->tarea, $contacto);
+                    $url = $this->sendWhatsappWebMessage();
 
-                    $this->mensajeRespuesta($respuesta, $this->tarea);
+                    $this->redirect($url);
+                    // $respuesta = $whatsApp->sendConfirmationClient($this->tarea, $contacto);
+
+
+                    // $this->mensajeRespuesta($respuesta, $this->tarea);
                 } else {
 
-                    $this->dispatch('not-number');
+
+                    $this->dispatch(
+                        'notify-toast',
+                        icon: 'error',
+                        title: 'ERROR AL ENVIAR',
+                        mensaje: 'El contacto no tiene un número de teléfono válido.',
+                    );
                 }
             } catch (\Throwable $th) {
                 $this->dispatch(
@@ -99,6 +109,48 @@ class ClienteContactos extends Component
                 mensaje: 'Ocurrio el sgte error: ' . $respuesta->responseData()['error']['message'],
             );
             return false;
+        }
+    }
+
+    public function sendWhatsappWebMessage()
+    {
+        if ($this->selected) {
+            try {
+                $contacto = Contactos::findOrFail($this->selected);
+                if ($contacto->telefono) {
+                    $tipoTarea = $this->tarea->tipo_tarea->nombre ?? '';
+                    $vehiculo = $this->tarea->vehiculo->placa ?? '';
+                    $cliente = $this->tarea->cliente->razon_social ?? $this->tarea->cliente->nombre ?? '';
+                    $fecha = ($this->tarea->fecha_hora ?? $this->tarea->fecha_programada)?->format('d/m/Y h:i A') ?? '';
+                    $uuid = $this->tarea->uuid;
+
+                    if ($uuid == null) {
+                        $this->dispatch(
+                            'notify-toast',
+                            icon: 'error',
+                            title: 'ERROR AL ENVIAR',
+                            mensaje: 'El uuid de la tarea no es valido.',
+                        );
+                    }
+
+                    $link = route('confirmacion.tarea', $this->tarea);
+                    $mensaje = "SERVICIO TECNICO FINALIZADO\n🛠$tipoTarea.\nEn el vehículo de placa: $vehiculo.🚗.\nEstimado Cliente: $cliente\npor favor dar conformidad al servicio brindado el Dia: \n$fecha\n🌐 Para confirmar haz clic en el botón:\n$link";
+                    $telefono = preg_replace('/[^0-9]/', '', $contacto->telefono);
+                    $url = 'https://web.whatsapp.com/send?phone=51' . $telefono . '&text=' . urlencode($mensaje);
+                    return $url;
+                    // $this->dispatch('open-modal', 'modal-notificacion');
+                    // $this->dispatch('notify-toast', icon: 'success', title: 'MENSAJE ENVIADO', mensaje: 'Se envio correctamente el mensaje al contacto');
+                } else {
+                    $this->dispatch('not-number');
+                }
+            } catch (\Throwable $th) {
+                $this->dispatch(
+                    'notify-toast',
+                    icon: 'error',
+                    title: 'ERROR AL ENVIAR',
+                    mensaje: 'Ocurrió el sgte error: ' . $th->getMessage(),
+                );
+            }
         }
     }
 }
