0db0f01cb972b455b8dc365175763d2fe9bf8a83
Jhamner Sifuentes Vasquez <jhamnerx1x@gmail.com>
2026-03-10 10:29:52 -0500

    update

--- resources/views/livewire/admin/clientes/clientes-index.blade.php
+++ resources/views/livewire/admin/clientes/clientes-index.blade.php
@@ -29,14 +29,9 @@
 
             <!-- Add customer button -->
             @can('crear-cliente')
-                <button wire:click='openModalSave'
-                    class="btn bg-gray-900 text-gray-100 hover:bg-gray-800 dark:bg-gray-100 dark:text-gray-800 dark:hover:bg-white">
-                    <svg class="w-4 h-4 fill-current shrink-0 xs:hidden" viewBox="0 0 16 16">
-                        <path
-                            d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
-                    </svg>
-                    <span class="max-xs:sr-only xs:block ml-2">Agregar Cliente</span>
-                </button>
+                <x-form.button wire:click='openModalSave' dark icon="plus">
+                    Agregar Cliente
+                </x-form.button>
             @endcan
 
         </div>
