6447affcb410af78a4074dbc057450a0be2ba4bd
Jhamner Sifuentes Vasquez <jhamnerx1x@gmail.com>
2026-06-15 15:23:06 -0500

    feat(wa): VerifyInternalToken middleware + kernel alias

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -73,6 +73,8 @@ class Kernel extends HttpKernel
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
+        // WhatsApp omnicanal
+        'whatsapp.internal' => \App\Http\Middleware\VerifyInternalToken::class,
 
     ];
 }
